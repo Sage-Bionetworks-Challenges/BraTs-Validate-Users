bd447575457d033bd3bc7c6649ed8b87832ed16e
verena <9377970+vpchung@users.noreply.github.com>
2023-05-24 18:43:04 -0700

    update frequency to every 5 minutes

--- runMonitor.R
+++ runMonitor.R
@@ -1,15 +1,22 @@
 source("config.R")
 require(cronR)
+
 # list the contents of a crontab
 cron_ls()
+
 # list the full path of where the rscript is located
 path <- "validateResponse.R"
+
 # Create a command to execute an R-script
 cmd <- cron_rscript(path, workdir = system("pwd", intern = TRUE))
+
 # add the command and specify the days/times to start
 cron_add(
-  command = cmd, frequency = "minutely", days_of_week = c(0:7),
-  id = "braTs", description = "validateJob"
+  command = cmd,
+  frequency = "*/5 * * * *",
+  id = "brats",
+  description = "Review requests every 15 minutes."
 )
+
 # kill the job by id
-cron_rm(id = "braTs")
+cron_rm(id = "brats")
